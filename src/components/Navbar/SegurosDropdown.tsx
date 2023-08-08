6032ae56c7ff319308aa4cd640469e945a94c28c
Thomas Albuquerque <tealbthomas@gmail.com>
2023-08-08 16:39:58 -0300

    Seguros' menu now is split in two parts for better ui/ux.

--- src/components/Navbar/SegurosDropdown.tsx
+++ src/components/Navbar/SegurosDropdown.tsx
@@ -62,11 +62,14 @@ const insurances = [
   },
 ];
 
+const firstHalftInsurances = insurances.slice(0, 6);
+const secondHalfInsurances = insurances.slice(6, 12);
+
 export default function SegurosDropdown() {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex w-full items-center justify-center rounded-md px-3 py-2  font-semibold text-secondary hover:bg-gray-50">
+        <Menu.Button className="inline-flex w-full items-center justify-center rounded-md   font-semibold text-secondary hover:bg-gray-50">
           Seguros
           <RxCaretDown className="-mr-1 h-5 w-5" aria-hidden="true" />
         </Menu.Button>
@@ -80,23 +83,43 @@ export default function SegurosDropdown() {
         leave="transition ease-in duration-75"
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95">
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-36 origin-top-right rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-2">
-            {insurances.map((insurance, index) => (
-              <Menu.Item key={index}>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={classNames(
-                      active ? ' text-primary' : 'text-secondary',
-                      'flex gap-2 px-4 py-2 text-sm items-center'
-                    )}>
-                    {insurance.icon}
-                    {insurance.title}
-                  </a>
-                )}
-              </Menu.Item>
-            ))}
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-80 origin-top-right rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-2 flex">
+            <div>
+              {firstHalftInsurances.map((insurance, index) => (
+                <Menu.Item key={index}>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={classNames(
+                        active ? ' text-primary' : 'text-secondary',
+                        'flex gap-2 px-4 py-2 text-sm items-center'
+                      )}>
+                      {insurance.icon}
+                      {insurance.title}
+                    </a>
+                  )}
+                </Menu.Item>
+              ))}
+            </div>
+
+            <div>
+              {secondHalfInsurances.map((insurance, index) => (
+                <Menu.Item key={index}>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={classNames(
+                        active ? ' text-primary' : 'text-secondary',
+                        'flex gap-2 px-4 py-2 text-sm items-center'
+                      )}>
+                      {insurance.icon}
+                      {insurance.title}
+                    </a>
+                  )}
+                </Menu.Item>
+              ))}
+            </div>
           </div>
         </Menu.Items>
       </Transition>
