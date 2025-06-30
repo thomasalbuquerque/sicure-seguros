e8612a17bbf53e40baa76aa765bcff03951a22be
Thomas Albuquerque <tealbthomas@gmail.com>
2025-06-29 22:29:26 -0300

    Refactor SegurosDropdown to use Next.js Link component for navigation and improve code consistency

--- src/components/Navbar/SegurosDropdown.tsx
+++ src/components/Navbar/SegurosDropdown.tsx
@@ -7,7 +7,6 @@ import { BiSolidBuildingHouse } from 'react-icons/bi';
 import { FaHeartCirclePlus } from 'react-icons/fa6';
 import { IoBody } from 'react-icons/io5';
 import { BsGlobeEuropeAfrica } from 'react-icons/bs';
-import { MdPedalBike } from 'react-icons/md';
 import { FaHouseUser } from 'react-icons/fa';
 import { BsAirplaneFill } from 'react-icons/bs';
 import { FaPlaneDeparture } from 'react-icons/fa';
@@ -15,50 +14,54 @@ import { LuBoxes } from 'react-icons/lu';
 import { LiaHorseHeadSolid } from 'react-icons/lia';
 
 import { RxCaretDown } from 'react-icons/rx';
+import Link from 'next/link';
 
 function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(' ');
 }
 
 const insurances = [
-  { title: 'Auto', icon: <FaTruckFront className="text-primary" /> },
-  { title: 'Residencial', icon: <IoHome className="text-primary" /> },
+  { title: 'Auto', icon: <FaTruckFront className='text-primary' />, link: '/seguro-automotivo' },
+  { title: 'Residencial', icon: <IoHome className='text-primary' />, link: '/seguro-residencial' },
   {
     title: 'Empresarial',
-    icon: <MdBusinessCenter className="text-primary" />,
+    icon: <MdBusinessCenter className='text-primary' />,
+    link: '/seguro-empresarial',
   },
-  { title: 'Vida', icon: <FaHeartCirclePlus className="text-primary" /> },
+  { title: 'Vida', icon: <FaHeartCirclePlus className='text-primary' />, link: '/seguro-de-vida' },
   {
     title: 'Aeronáutico (Casco)',
-    icon: <FaPlaneDeparture className="text-primary" />,
+    icon: <FaPlaneDeparture className='text-primary' />,
+    link: '/seguro-aeronautico-casco',
   },
   {
     title: 'Aeronáutico (R.E.T.A)',
-    icon: <BsAirplaneFill className="text-primary" />,
+    icon: <BsAirplaneFill className='text-primary' />,
+    link: '/seguro-aeronautico-reta',
   },
   {
     title: 'Acidentes',
-    icon: <IoBody className="text-primary" />,
+    icon: <IoBody className='text-primary' />,
   },
   {
     title: 'Aluguel',
-    icon: <FaHouseUser className="text-primary" />,
+    icon: <FaHouseUser className='text-primary' />,
   },
   {
     title: 'Viagem',
-    icon: <BsGlobeEuropeAfrica className="text-primary" />,
+    icon: <BsGlobeEuropeAfrica className='text-primary' />,
   },
   {
     title: 'Cavalo',
-    icon: <LiaHorseHeadSolid className="text-primary" />,
+    icon: <LiaHorseHeadSolid className='text-primary' />,
   },
   {
     title: 'Condomínio',
-    icon: <BiSolidBuildingHouse className="text-primary" />,
+    icon: <BiSolidBuildingHouse className='text-primary' />,
   },
   {
     title: 'Diversos',
-    icon: <LuBoxes className="text-primary" />,
+    icon: <LuBoxes className='text-primary' />,
   },
 ];
 
@@ -76,39 +79,37 @@ export default function SegurosDropdown() {
     }
   };
   return (
-    <Menu as="div" className="relative inline-block text-left">
+    <Menu as='div' className='relative inline-block text-left'>
       <div>
-        <Menu.Button className="inline-flex w-full items-center justify-center rounded-md   font-semibold text-secondary">
+        <Menu.Button className='inline-flex w-full items-center justify-center rounded-md   font-semibold text-secondary'>
           Seguros
-          <RxCaretDown className="-mr-1 h-5 w-5" aria-hidden="true" />
+          <RxCaretDown className='-mr-1 h-5 w-5' aria-hidden='true' />
         </Menu.Button>
       </div>
 
       <Transition
         as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95">
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-80 origin-top-right rounded-md shadow-lg bg-bgColor ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-2 flex">
+        enter='transition ease-out duration-100'
+        enterFrom='transform opacity-0 scale-95'
+        enterTo='transform opacity-100 scale-100'
+        leave='transition ease-in duration-75'
+        leaveFrom='transform opacity-100 scale-100'
+        leaveTo='transform opacity-0 scale-95'
+      >
+        <Menu.Items className='absolute right-0 z-10 mt-2 w-80 origin-top-right rounded-md shadow-lg bg-bgColor ring-1 ring-black ring-opacity-5 focus:outline-none'>
+          <div className='py-2 flex'>
             <div>
               {firstHalftInsurances.map((insurance, index) => (
                 <Menu.Item key={index}>
                   {({ active }) => (
                     // <a href="insurance.path"
-                    <div
-                      className={classNames(
-                        active ? ' text-primary' : 'text-secondary',
-                        'flex gap-2 px-4 py-2 text-sm items-center cursor-pointer'
-                      )}
-                      onClick={scrollToSection}>
-                      {insurance.icon}
-                      {insurance.title}
-                      {/*</a> */}
-                    </div>
+                    <Link href={insurance.link ?? '/'}>
+                      <div className={classNames(active ? ' text-primary' : 'text-secondary', 'flex gap-2 px-4 py-2 text-sm items-center cursor-pointer')}>
+                        {insurance.icon}
+                        {insurance.title}
+                        {/*</a> */}
+                      </div>
+                    </Link>
                   )}
                 </Menu.Item>
               ))}
@@ -119,16 +120,13 @@ export default function SegurosDropdown() {
                 <Menu.Item key={index}>
                   {({ active }) => (
                     // <a href="insurance.path"
-                    <div
-                      className={classNames(
-                        active ? ' text-primary' : 'text-secondary',
-                        'flex gap-2 px-4 py-2 text-sm items-center cursor-pointer'
-                      )}
-                      onClick={scrollToSection}>
-                      {insurance.icon}
-                      {insurance.title}
-                      {/*</a> */}
-                    </div>
+                    <Link href={insurance.link ?? '/'}>
+                      <div className={classNames(active ? ' text-primary' : 'text-secondary', 'flex gap-2 px-4 py-2 text-sm items-center cursor-pointer')}>
+                        {insurance.icon}
+                        {insurance.title}
+                        {/*</a> */}
+                      </div>
+                    </Link>
                   )}
                 </Menu.Item>
               ))}
